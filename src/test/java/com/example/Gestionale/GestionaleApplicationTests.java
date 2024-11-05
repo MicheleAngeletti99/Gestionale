219edc787acf9da9a1531a7c818fcd5cbb4aa847
Michele Angeletti <m.angeletti99@gmail.com>
2024-11-05 12:09:17 +0100

    Primo commit

---
+++ src/test/java/com/example/Gestionale/GestionaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestionale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
